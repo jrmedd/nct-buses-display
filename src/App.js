6875840e0ef0297235f870a7d20c5669b15d3336
jrmedd <hello@jamesmedd.co.uk>
2020-11-24 17:43:54 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { FlapDisplay, Presets } from "react-split-flap-effect";
+import styled, {css } from "styled-components";
+import useInterval from "./useInterval";
+import "./style.css"
 
-function App() {
+const App = () => {
+  const stopId = new URL(window.location).searchParams.get("stop");
+  const [busTimetable, setBusTimetable] = React.useState({'buses':[]})
+  React.useEffect(()=>{
+    getBus(stopId).then(data=>setBusTimetable(data))
+  }, [])
+  useInterval(()=>{
+    getBus(stopId).then(data=>setBusTimetable(data))
+  }, 15000)
+  if (stopId) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {busTimetable.buses.map(bus=><JustifiedBusTime length={50} busTime={bus} />)}
+    </main>
   );
+  }
+  else {
+    return(<main>
+      <FlapDisplay className="XL" chars={Presets.ALPHANUM} length="50" value="No stop provided" />
+    </main>)
+  }
+}
+
+const JustifiedBusTime = props => {
+  const number = props.busTime.number;
+  const destination = props.busTime.destination;
+  const due = props.busTime.due;
+  const spacing = (props.length) - (number.length + destination.length + due.length);
+  const shortSpace = (" ").repeat(8)
+  const longSpace = (" ").repeat(spacing-shortSpace.length);
+  const justifiedValue = `${number}${shortSpace}${destination}${longSpace}${due}`;
+  return (
+    <FlapDisplay className="XL" chars={Presets.ALPHANUM} padMode="start" length={props.length} value={justifiedValue} />
+  )
+}
+
+const getBus = async stop => {
+    const result = await fetch(`http://127.0.0.1:5000/stop/${stop}`).then(res=>res.json())
+    return(result)
 }
 
 export default App;
